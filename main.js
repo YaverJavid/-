3804521f143b333303cc1b8947261b1efab05913
YaverJavid <lindamoplhooon@gmail.com>
2021-08-02 21:37:36 +0530

    Sucess!!

---
+++ main.js
@@ -0,0 +1,5 @@
+/*setInterval(()=>{
+    var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+    console.log(scrollTop)
+},1000)
+scrollTo()*/
